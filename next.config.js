5e294a6b8afefb4276b48379cd8d496882a4ea32
Tyronwilliam <tyronwilliamchanu@gmail.com>
2022-02-25 19:30:19 +0100

    next config fix

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibWF1am9qby1qbyIsImEiOiJjbDAyZmgzcW4wMGtwM2pwMjg3cG5nOXdhIn0.MW1qeoiiyl3LKew2yOlr1g",
+  },
+};
